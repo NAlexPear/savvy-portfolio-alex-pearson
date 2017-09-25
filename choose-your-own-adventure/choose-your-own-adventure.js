d30c8e29a9a751a86bc79f7fbef7a317a882e279
Alex Pearson <alex@alexpear.com>
2017-09-25 18:19:09 -0500

    Add first story-level abstractions

--- choose-your-own-adventure/choose-your-own-adventure.js
+++ choose-your-own-adventure/choose-your-own-adventure.js
@@ -1,16 +1,34 @@
 var story = {
-    "start": "You find yourself looking over a vast desert, low on water. Would you like to dig for water where you stand, or would you like to search for an oasis?"
+    "start": "You find yourself looking over a vast desert, low on water. Would you like to dig for water where you stand, or would you like to search for an oasis? Type 'dig' or 'search', please.",
+    "dig": "You take off one of your shoes to use as a shovel, and promptly burn your toes. It's uncomfortable, and also you run out of water and are never heard from again.",
+    "search": "You head east for some reason. Then you see a cloud of dust! Would you like to investigate, or turn to the north? Type 'investigate' or 'turn'",
+    "investigate": "investigate was called!",
+    "turn": "turn was called!"
 };
 
-function runStory(){
-    var userInput = prompt( story.start );
+/* eslint-disable complexity */
+function runStory( node ){
+    var userInput = prompt( story[node] );
 
-    if( !userInput ){
-        runStory();
+    if( userInput === "dig" ){
+        alert( story.dig );
+    }
+    else if( userInput === "search" ){
+        userInput = prompt( story.search );
+
+        if( userInput === "investigate" ){
+            alert( story.investigate );
+        }
+        else if( userInput === "turn" ){
+            alert( story.turn );
+        }
+        else{
+            runStory();
+        }
     }
     else{
-        // user inputs some valid story node
+        runStory( node );
     }
 }
 
-runStory();
+runStory( "start" );
