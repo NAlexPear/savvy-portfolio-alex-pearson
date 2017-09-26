f5f7d7bca0101e8b129f6152e04c6a7fdc66b65b
Alex Pearson <alex@alexpear.com>
2017-09-25 20:49:27 -0500

    Refactor choose-your-own-adventure runStory function

--- choose-your-own-adventure/choose-your-own-adventure.js
+++ choose-your-own-adventure/choose-your-own-adventure.js
@@ -1,33 +1,41 @@
 var story = {
-    "start": "You find yourself looking over a vast desert, low on water. Would you like to dig for water where you stand, or would you like to search for an oasis? Type 'dig' or 'search', please.",
-    "dig": "You take off one of your shoes to use as a shovel, and promptly burn your toes. It's uncomfortable, and also you run out of water and are never heard from again.",
-    "search": "You head east for some reason. Then you see a cloud of dust! Would you like to investigate, or turn to the north? Type 'investigate' or 'turn'",
-    "investigate": "investigate was called!",
-    "turn": "turn was called!"
+    "start": {
+        "prompt": "You find yourself looking over a vast desert, low on water. Would you like to dig for water where you stand, or would you like to search for an oasis? Type 'dig' or 'search', please.",
+        "choices": [ "dig", "search" ]
+    },
+    "dig": {
+        "prompt": "You take off one of your shoes to use as a shovel, and promptly burn your toes. It's uncomfortable, and also you run out of water and are never heard from again.",
+    },
+    "search": {
+        "prompt": "You head east for some reason. Then you see a cloud of dust! Would you like to investigate, or turn to the north? Type 'investigate' or 'turn'",
+        "choices": [ "investigate", "turn" ]
+    },
+    "investigate": {
+        "prompt": "investigate was called!",
+    },
+    "turn": {
+        "prompt": "turn was called!"
+    }
 };
 
 /* eslint-disable complexity */
 function runStory( node ){
-    var userInput = prompt( story[node] );
+    var branch = story[node];
+    var choices = branch.choices;
+    var userInput;
 
-    if( userInput === "dig" ){
-        alert( story.dig );
-    }
-    else if( userInput === "search" ){
-        userInput = prompt( story.search );
-
-        if( userInput === "investigate" ){
-            alert( story.investigate );
-        }
-        else if( userInput === "turn" ){
-            alert( story.turn );
-        }
-        else{
-            runStory();
-        }
+    if( !choices ){
+        alert( branch.prompt );
     }
     else{
-        runStory( node );
+        userInput = prompt( branch.prompt );
+
+        if( userInput === choices[0] || userInput === choices[1] ){
+            runStory( userInput );
+        }
+        else{
+            runStory( node );
+        }
     }
 }
 
