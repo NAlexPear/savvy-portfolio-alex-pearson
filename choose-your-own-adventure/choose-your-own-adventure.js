2a1bb6c7a2e85e08fc17df33f405fdabdf9bd710
Alex Pearson <alex@alexpear.com>
2017-09-25 20:58:05 -0500

    Flesh out epic desert adventure

--- choose-your-own-adventure/choose-your-own-adventure.js
+++ choose-your-own-adventure/choose-your-own-adventure.js
@@ -11,10 +11,17 @@ var story = {
         "choices": [ "investigate", "turn" ]
     },
     "investigate": {
-        "prompt": "investigate was called!",
+        "prompt": "As you draw closer, you notice a herd of camels. But where are their riders? Should you approach, or watch from a distance in case of ambush? Type 'approach' or 'wait' to continue",
+        "choices": [ "approach", "wait" ]
     },
     "turn": {
-        "prompt": "turn was called!"
+        "prompt": "You turn around (uh-oh), and find that Der Komissar's in town (uh-oh!). You continue dancing into the sunset."
+    },
+    "approach": {
+        "prompt": "As you get close, you are trampled by camels. A few of them also spit on you. It's very humiliating, and terminal."
+    },
+    "wait": {
+        "prompt": "What could you possibly be waiting for? It's a desert! You run out of water, thinking of how delicious camel must taste instead of sand."
     }
 };
 
