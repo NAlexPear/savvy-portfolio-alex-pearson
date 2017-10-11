f8d6c27008a3db11692c2e7affdc05430bfca006
Alex Pearson <alex@alexpear.com>
2017-10-10 21:44:38 -0500

    Add author-fetching JS

--- blog/script.js
+++ blog/script.js
@@ -1,26 +1,58 @@
 /* globals $, _ */
-var postRequest = $.ajax( "https://jsonplaceholder.typicode.com/posts" );
+var host = "https://jsonplaceholder.typicode.com";
+var postRequest = $.ajax( `${host}/posts` );
 var $posts = $( "#posts" );
 
 function templatePost( post ){
     return `
       <div class="post">
         <h3>${post.title}</h3>
-        <h4>Written By: ${post.userId}</h4>
+        <h4>
+          Written By: <span class="author-${post.userId}"></span>
+        </h4>
         <p>${post.body}</p>
       </div>
     `;
 }
 
-function appendPosts( posts ){
-    var firstFive = _
-        .take( posts, 5 )
-        .reduce(
-            ( output, post ) => output + templatePost( post ),
-            ""
-        );
+function templatePosts( posts, post ){
+    var newPost = templatePost( post );
 
-    $posts.append( firstFive );
+    return `${posts}${newPost}`;
+}
+
+function getAuthor( requests, post ){
+    var id = post.userId;
+
+    if( !requests[id] ){
+        requests[id] = $.ajax( `${host}/users/${id}` );
+    }
+
+    return requests;
+}
+
+function injectAuthors( authors ){
+    authors.forEach( ( author ) => {
+        var $targets = $( `.author-${author.id}` );
+
+        if( $targets.length ){
+            $targets.text( author.name );
+        }
+    } );
+}
+
+function appendPosts( posts ){
+    var firstFive = _.take( posts, 5 );
+    var requests = firstFive.reduce( getAuthor, {} );
+    var markup = firstFive.reduce( templatePosts, "" );
+
+    var requestPromises = Object.values( requests );
+
+    $posts.append( markup );
+
+    Promise
+        .all( requestPromises )
+        .then( injectAuthors );
 }
 
 postRequest.then( appendPosts );
