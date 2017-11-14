74ca007004ca34913aea3d30cce9ad101abe343b
Alex Pearson <alex@alexpear.com>
2017-11-13 20:52:16 -0600

    Add personalized greeting

--- js/greeting.js
+++ js/greeting.js
@@ -1,12 +1,16 @@
-var hello = "hello";
-var userName = prompt( "What's your name?" );
-var checkUserName = function checkUserName(){
+var originalName = prompt( "What's your name?" );
+
+var checkUserName = function checkUserName( userName ){
     if( !userName || userName === " " ){
         userName = prompt( "What's your name, for real this time?" );
-        checkUserName();
+
+        checkUserName( userName );
+    }
+    else{
+        originalName = userName;
     }
 };
 
-checkUserName();
+checkUserName( originalName );
 
-alert( hello + " " + userName );
+document.querySelector( "#subheader" ).textContent = "Hello, " + originalName;
