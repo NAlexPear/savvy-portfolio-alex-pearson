33c3235ca87828dc395e19df593ff6fd22cb9d57
Alex Pearson <alex@alexpear.com>
2017-11-15 21:02:55 -0600

    Add runStory

--- js/choose-your-own-adventure.js
+++ js/choose-your-own-adventure.js
@@ -1,39 +1,43 @@
 var story = {
-    "start": "You find yourself suddenly teleported to an interdimensional hub filled with arthropod-like creatures that seem to ignore you. Would you like to travel further into the Hive or make a break for whatever appears to be 'outside'?",
-    "hive": "Knowing nothing about arthopodian architecture, you meander aimlessly through corridors innumerable and unspeakably vast. As you near exhaustion, you finally see it: a font of what appears to be water! Would you like to take a drink or keep walking?",
-    "outside": "As you step outside the Hive, a mysterious force field draws back, with the effect of ejecting you violently into space. As you asphyxiate, you recall fondly that scene from Guardians of the Galaxy 2 (no spoilers)",
-    "drink": "You take a drink of the mysterious alien liquid and promptly die because it's pretty much poison, duh.",
-    "walk": "You walk farther into the hive, but then get eaten, lol."
+    "start": {
+        "text": "You find yourself suddenly teleported to an interdimensional hub filled with arthropod-like creatures that seem to ignore you. Would you like to travel further into the Hive or make a break for whatever appears to be 'outside'?",
+        "choices": [ "hive", "outside" ]
+    },
+    "hive": {
+        "text": "Knowing nothing about arthopodian architecture, you meander aimlessly through corridors innumerable and unspeakably vast. As you near exhaustion, you finally see it: a font of what appears to be water! Would you like to take a drink or keep walking?",
+        "choices": [ "walk", "drink" ]
+    },
+    "outside": {
+        "text": "As you step outside the Hive, a mysterious force field draws back, with the effect of ejecting you violently into space. As you asphyxiate, you recall fondly that scene from Guardians of the Galaxy 2 (no spoilers)",
+    },
+    "drink": {
+        "text": "You take a drink of the mysterious alien liquid and promptly die because it's pretty much poison, duh.",
+    },
+    "walk": {
+        "text": "You walk farther into the hive, but then get eaten, lol."
+    }
 };
 
-var runStory = function runStory(){
-    var choice = prompt( story.start );
+var runStory = function runStory( branch ){
+    var chapter = story[branch];
+    var choices = chapter.choices;
+    var choice;
 
-    if( choice === "hive" ){
-        choice = prompt( story[choice] );
+    if( choices ){
+        choice = prompt( chapter.text );
 
-        if( choice === "drink" ){
-            document
-                .querySelector( "#output" )
-                .textContent = story[choice];
-        }
-        else if( choice === "walk" ){
-            document
-                .querySelector( "#output" )
-                .textContent = story[choice];
+        if( choice === choices[0] || choice === choices[1] ){
+            runStory( choice );
         }
         else{
-            // handle bad input
+            runStory( branch );
         }
     }
-    else if( choice === "outside" ){
+    else{
         document
             .querySelector( "#output" )
-            .textContent = story[choice];
-    }
-    else{
-        runStory();
+            .textContent = chapter.text;
     }
 };
 
-runStory();
+runStory( "start" );
