0808e77b9b8915d737819086b80c55bfb05f7ccd
Alex Pearson <alex@alexpear.com>
2019-02-18 18:47:52 -0600

    :sparkles: implement recursive solution

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var nameChecker = function nameChecker(){
     var name = prompt('Please enter your name!');
 
-    // Check for a blank name!
     if(name === ''){
-        name = prompt('For real, enter your name!');
+        nameChecker();
     }
     else{
         document.querySelector('h1').textContent = `Hello, ${name}`;
