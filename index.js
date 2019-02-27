d5db6da1d929f33e66051efe9d0fee91d1ce8823
Alex Pearson <alex@alexpear.com>
2019-02-26 19:51:27 -0600

    :checkered_flag: Implement basic uni-direction Single-Page App routing

--- index.js
+++ index.js
@@ -4,11 +4,52 @@ import Header from './src/Header';
 import Navigation from './src/Navigation';
 
 
+var State = {
+    'Home': {
+        'title': 'Welcome to Alex\'s Savvy Coder Website'
+    },
+    'Blog': {
+        'title': 'Read my words and stuff'
+    },
+    'Contact': {
+        'title': 'Contact Alex'
+    },
+    'Projects': {
+        'title': 'Behold my works, ye mighty, and despair'
+    }
+};
+
 var root = document.querySelector('#root');
 
-root.innerHTML = `
-  ${Navigation()}
-  ${Header('Welcome to my Savvy Coders website!')}
-  ${Content()}
-  ${Footer()}
-`;
+function render(state){
+    var links;
+
+    root.innerHTML = `
+      ${Navigation(state)}
+      ${Header(state.title)}
+      ${Content(state)}
+      ${Footer(state)}
+    `;
+
+    links = document.querySelectorAll('#navigation > ul > li > a');
+
+    links[0].addEventListener('click', (event) => {
+        event.preventDefault();
+
+        render(State.Blog);
+    });
+
+    links[1].addEventListener('click', (event) => {
+        event.preventDefault();
+
+        render(State.Contact);
+    });
+
+    links[2].addEventListener('click', (event) => {
+        event.preventDefault();
+
+        render(State.Projects);
+    });
+}
+
+render(State.Home);
