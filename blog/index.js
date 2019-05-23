35d58aad9e75fd666a84eee5da902e12e39b783a
Alex Pearson <alex@alexpear.com>
2019-05-22 20:59:13 -0500

    :sparkles: Add blog post parsing

---
+++ blog/index.js
@@ -0,0 +1,44 @@
+const posts = [
+    {
+        'author': 'Benedict Cumberbund',
+        'title': 'Celebrity Crime Skyrockets!',
+        'blurb': 'Lorem ipsum dolor sit amet'
+    },
+    {
+        'author': 'Benedict Cumberbund',
+        'title': 'I look like an otter',
+        'blurb': 'Lorem ipsum dolor sit amet'
+    },
+    {
+        'author': 'Benedict Cumberbund',
+        'title': 'Dr. Strange: A retrospective',
+        'blurb': 'Lorem ipsum dolor sit amet'
+    },
+    {
+        'author': 'Agatha Christie',
+        'title': 'Murder on the JavaScript Express',
+        'blurb': 'Lorem ipsum dolor sit amet'
+    }
+];
+
+function Post(post){
+    return `
+      <div>
+        <h2>${post.title}</h2>
+        <h3>by: ${post.author}</h3>
+        <p>${post.blurb}</p>
+      </div>
+    `;
+}
+
+function Content(items){
+    let content = '';
+
+    for(let i = 0; i < items.length; i++){
+        content = `${content}${Post(items[i])}`;
+    }
+
+    return content;
+}
+
+document.querySelector('#content').innerHTML = Content(posts);
